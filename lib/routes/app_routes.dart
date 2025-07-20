b084b9e070fc93f16f3625ab3906f2ef9db08f14
Palash-Chandra-Roy <palashchandra900189@gmail.com>
2025-07-20 09:05:29 +0600

    Splash, Name, user

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,12 +1,16 @@
 import 'package:go_router/go_router.dart';
+import 'package:market_jango/features/auth/screens/name_screen.dart';
 import 'package:market_jango/features/auth/screens/new_password_screen.dart';
+import 'package:market_jango/features/auth/screens/phone_number.dart';
+import 'package:market_jango/features/auth/screens/splash_screen.dart';
+import 'package:market_jango/features/auth/screens/user.dart';
 import 'package:market_jango/features/auth/screens/verification_screen.dart';
 
 import '../features/auth/screens/forgot_password_screen.dart';
 import '../features/auth/screens/login_screen.dart';
 
 final GoRouter router = GoRouter(
-  initialLocation: LoginScreen.routeName,
+  initialLocation: SplashScreen.routeName,
   routes: [
     GoRoute(
       path: LoginScreen.routeName,
@@ -28,5 +32,21 @@ final GoRouter router = GoRouter(
       name: 'new_password',
       builder: (context, state) => const NewPasswordScreen(),
     ),
+    GoRoute(path:SplashScreen.routeName, 
+    name: 'splash',
+    builder: (context,state)=>const SplashScreen(), 
+     ),
+     GoRoute(path:NameScreen.routeName, 
+    name: 'name',
+    builder: (context,state)=>const NameScreen(), 
+     ),
+      GoRoute(path:UserScreen.routeName, 
+    name: 'user',
+    builder: (context,state)=>const UserScreen(), 
+     ),
+     GoRoute(path:PhoneNumberScreen.routeName, 
+    name: 'phoneNumber',
+    builder: (context,state)=>const PhoneNumberScreen(), 
+     )
   ],
 );
