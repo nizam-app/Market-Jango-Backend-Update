bc0c56fe6e51875247043e811f5ff3a854d357fe
Palash Chandra Roy <164641475+shahedroks@users.noreply.github.com>
2025-11-14 03:44:33 +0600

    dettaal screen

--- lib/core/constants/api_control/transport_api.dart
+++ lib/core/constants/api_control/transport_api.dart
@@ -3,4 +3,5 @@ import 'global_api.dart';
 class TransportAPIController {
   static String _base_api = "$api/api";
   static String approved_driver = "$_base_api/approved-driver";
+  static String all_order_transport = "$_base_api/all-order/transport";
 }
